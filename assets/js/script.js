e46b92f834515979447e1df57ba24f151fd70fb6
sarebok <bortonae@hotmail.com>
2023-08-06 18:57:28 -0300

    add total price calculation funcionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-const notebookPrice = document.querySelector("#notebook-price");
+const productPrice = document.querySelector("#product-price");
+console.log(productPrice);
+const inputProductQuantity = document.querySelector("#input-product-quantity");
+console.log(inputProductQuantity);
+const inputColor = document.querySelector("#input-color");
+console.log(inputColor);
+const buyBtn = document.querySelector("#buy-button");
+console.log(buyBtn);
+
+buyBtn.addEventListener("click", calculate);
+
+function calculate() {
+  const a = inputProductQuantity.value;
+  const totalPrice = Number(productPrice.innerHTML) * Number(a);
+  const calculatedTotalPrice = document.querySelector(
+    "#calculated-total-price"
+  );
+  calculatedTotalPrice.innerHTML = totalPrice;
+}
