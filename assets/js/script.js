0cd157fe98a54191acdd844ed7462af9a8992613
sarebok <bortonae@hotmail.com>
2023-08-06 18:14:59 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const notebookPrice = document.querySelector("#notebook-price");
+
+const precioNuevo = 100;
+
+notebookPrice.innerHTML = precioNuevo;
+const b = document.querySelector("#btn");
+
+b.addEventListener("click", console.log("holi"));
