9e8181f825d1c35b534ea592c38efdae002f3acb
sarebok <bortonae@hotmail.com>
2023-08-06 18:33:28 -0300

    add main card structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,1 @@
 const notebookPrice = document.querySelector("#notebook-price");
-
-const precioNuevo = 100;
-
-notebookPrice.innerHTML = precioNuevo;
-const b = document.querySelector("#btn");
-
-b.addEventListener("click", console.log("holi"));
