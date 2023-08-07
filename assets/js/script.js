def59904814e1d1dc2550e6bceef61f6c452e993
sarebok <bortonae@hotmail.com>
2023-08-06 23:28:34 -0300

    add color selector funcionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,23 @@ const buyBtn = document.querySelector("#buy-button");
 buyBtn.addEventListener("click", calculate);
 
 function calculate() {
+  //calcular multiplicacion entre el numero inputado y el precio del pc
   const a = inputProductQuantity.value;
   const totalPrice = Number(productPrice.innerHTML) * Number(a);
+  //asignar valor calculado a calculated-total-price
   const calculatedTotalPrice = document.querySelector(
     "#calculated-total-price"
   );
   calculatedTotalPrice.innerHTML = totalPrice;
+
+  //asignar valor de cantidad inputada a calculated-quantity
+
+  const calculatedQuantity = document.querySelector("#calculated-quantity");
+  calculatedQuantity.innerHTML = a;
+  const cardManip = document.querySelector("#card-js");
+  cardManip.style.borderColor = inputColor.value;
+  //incluir el color en texto
+  const selectedColor = document.querySelector("#selected-color");
+  selectedColor.innerHTML = inputColor.value;
+  selectedColor.style.color = inputColor.value;
 }
